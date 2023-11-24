2389c98aabf1e41cff995b5db19564a8546cfb66
Tanvir Ahmed Aadeef <39330876+Tanxinaz@users.noreply.github.com>
2023-11-24 22:22:06 +0600

    Update storage.sol

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract BanglaTakaStorage {
+contract ElevateXStorage {
     mapping(address => uint256) public balanceOf;
     uint256 public totalSupply;
 
